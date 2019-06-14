771502f16a7cfde6ea1b9d12437a1003a206a42e
R-Escher <38119826+R-Escher@users.noreply.github.com>
2019-06-14 15:22:06 -0300

    Tela de login de paciente, medicos e laboratorio

---
+++ index-include/sidenav.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="pt-BR">
+    <head>
+        <title>Medicine System</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
+        <link href="css/simple-sidebar.css" rel="stylesheet">         
+    </head>
+    <body>
+        
+        <div class="d-flex" id="wrapper">
+
+            <!-- Sidebar -->
+            <div class="bg-light border-right" id="sidebar-wrapper">
+                <div class="sidebar-heading">Medicine System</div>
+                <div class="list-group list-group-flush">
+                <a href="#" class="list-group-item list-group-item-action bg-light">Paciente</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Médico</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Laboratório</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Administração</a>
+                </div>
+            </div>
+            <!-- /#sidebar-wrapper -->
+        
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+        
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+            
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+            
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        </ul>
+                    </div>
+                </nav>
+            
+                  
--- index.php
+++ index.php
@@ -1,36 +1,126 @@
-<?php 
- 
-// Transformando arquivo XML em Objeto
-$xml = simplexml_load_file('vendas.xml');
- 
-// Exibe as informações do XML
-echo 'Título: ' . $xml->titulo . '<br>';
-echo 'Data de Atualização: ' . $xml->data_atualizacao . '<br>';
- 
+<?php include 'index-include/sidenav.php'; ?>
+<!-- Tags em aberto: <html>, <body>, <div class="d-flex" id="wrapper">, <div id="page-content-wrapper"> -->
 
-function pesquisaNome ($xml,$nome){	
-	// Percorre todos os registros de vendas
-	foreach($xml->venda as $registro):
-		//FILTRO PRA BUSCA TODOS COM O RESPECTIVO $nome
-		if (stripos($registro->cliente, $nome) !== false){
-			echo 'Código da Venda: ' . $registro->cod_venda . '<br>';
-			echo 'Cliente: ' . $registro->cliente . '<br>';
-			echo 'Email: ' . $registro->email . '<br>';
+<div class="container">
+  
+  <!-- LOGIN PACIENTE -->
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <h5 class="card-title text-center">Paciente</h5>
+          <form class="form-signin">
+            <div class="form-label-group">
+              <br>
+              <label for="inputEmail">Digite seu CPF</label>
+              <input type="email" id="inputEmail" class="form-control" placeholder="Ex: 123456789-00" required autofocus>
+            </div>
 
-			// Percorre todos os itens da venda
-			foreach($registro->itens->item as $item):
-				echo 'Código do Produto: ' . $item->cod_produto . '<br>';
-				echo 'Quantidade: ' . $item->qtde . '<br>';
-				echo 'Descrição do Produto: ' . $item->descricao . '<br>';
-			endforeach;
-			echo '<hr>';
-		}
+            <div class="form-label-group">
+              <label for="inputPassword">Digite sua senha</label>
+              <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+            </div>
+            <br>
+            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
-	endforeach;
-}
-//MODIFICA VALOR E DEPOIS GRAVA NO XML
-//$xml->venda->cod_venda = 002; 
-//$xml->asXML('vendas.xml');
-pesquisaNome($xml,'joão')
+  <!-- LOGIN MEDICO -->
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <h5 class="card-title text-center">Médico</h5>
+          <form class="form-signin">
+            <div class="form-label-group">
+              <br>
+              <label for="inputEmail">Digite seu CRM</label>
+              <input type="email" id="inputEmail" class="form-control" placeholder="Ex: procurar padrao CRM" required autofocus>
+            </div>
 
-?>
+            <div class="form-label-group">
+              <label for="inputPassword">Digite sua senha</label>
+              <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+            </div>
+            <br>
+            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- LOGIN PACIENTE -->
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <h5 class="card-title text-center">Laboratório</h5>
+          <form class="form-signin">
+            <div class="form-label-group">
+              <br>
+              <label for="inputEmail">Digite seu CNPJ</label>
+              <input type="email" id="inputEmail" class="form-control" placeholder="Ex: procurar padrão CNPJ" required autofocus>
+            </div>
+
+            <div class="form-label-group">
+              <label for="inputPassword">Digite sua senha</label>
+              <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+            </div>
+            <br>
+            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div> <!-- / id="page-content-wrapper" | todo o conteúdo deve ficar aqui dentro -->
+</div> <!-- / class="d-flex" id="wrapper" -->
+  <!-- Javascript/Jquery functions for Bootstrap internal use -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
+
+  <!-- Menu Toggle Script--> 
+  <script>
+          $("#menu-toggle").click(function(e) {
+          e.preventDefault();
+          $("#wrapper").toggleClass("toggled");
+          });
+  </script>        
+        </body>
