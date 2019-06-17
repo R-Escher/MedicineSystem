94fa503b0f35cff5ffbce6e7493663ea8b9c7819
R-Escher <38119826+R-Escher@users.noreply.github.com>
2019-06-16 23:36:05 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 <div class="container">
   
   <!-- LOGIN PACIENTE -->
-  <div class="row">
+  <div class="row" id="paciente-login">
     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
       <div class="card card-signin my-5">
         <div class="card-body">
           <h5 class="card-title text-center">Paciente</h5>
           <form class="form-signin">
             <div class="form-label-group">
-              <br>
-              <label for="inputEmail">Digite seu CPF</label>
-              <input type="email" id="inputEmail" class="form-control" placeholder="Ex: 123456789-00" required autofocus>
+              
+              <label for="inputCpf">Digite seu CPF</label>
+              <input type="" id="inputCpf" class="form-control" placeholder="Ex: 123456789-00" required autofocus>
             </div>
 
             <div class="form-label-group">
               <label for="inputPassword">Digite sua senha</label>
               <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
             </div>
-            <br>
+            
             <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
           </form>
         </div>
@@ -29,23 +29,23 @@
   </div>
 
   <!-- LOGIN MEDICO -->
-  <div class="row">
+  <div class="row" id="medico-login">
     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
       <div class="card card-signin my-5">
         <div class="card-body">
           <h5 class="card-title text-center">Médico</h5>
           <form class="form-signin">
             <div class="form-label-group">
-              <br>
-              <label for="inputEmail">Digite seu CRM</label>
-              <input type="email" id="inputEmail" class="form-control" placeholder="Ex: procurar padrao CRM" required autofocus>
+              
+              <label for="inputCrm">Digite seu CRM</label>
+              <input type="" id="inputCrm" class="form-control" placeholder="Ex: procurar padrao CRM" required autofocus>
             </div>
 
             <div class="form-label-group">
               <label for="inputPassword">Digite sua senha</label>
               <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
             </div>
-            <br>
+            
             <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
           </form>
         </div>
@@ -53,30 +53,55 @@
     </div>
   </div>
 
-  <!-- LOGIN PACIENTE -->
-  <div class="row">
+  <!-- LOGIN LABORATORIO -->
+  <div class="row" id="laboratorio-login">
     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
       <div class="card card-signin my-5">
         <div class="card-body">
           <h5 class="card-title text-center">Laboratório</h5>
           <form class="form-signin">
             <div class="form-label-group">
-              <br>
-              <label for="inputEmail">Digite seu CNPJ</label>
-              <input type="email" id="inputEmail" class="form-control" placeholder="Ex: procurar padrão CNPJ" required autofocus>
+              
+              <label for="inputCnpj">Digite seu CNPJ</label>
+              <input type="" id="inputCnpj" class="form-control" placeholder="Ex: procurar padrão CNPJ" required autofocus>
             </div>
 
             <div class="form-label-group">
               <label for="inputPassword">Digite sua senha</label>
               <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
             </div>
-            <br>
+            
             <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
           </form>
         </div>
       </div>
     </div>
   </div>
+
+  <!-- LOGIN ADMIN -->
+  <div class="row" id="admin-login">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <h5 class="card-title text-center">Administração</h5>
+          <form class="form-signin">
+            <div class="form-label-group">
+              
+              <label for="inputLogin">Digite seu login</label>
+              <input type="email" id="inputLogin" class="form-control" placeholder="Ex: admin" required autofocus>
+            </div>
+
+            <div class="form-label-group">
+              <label for="inputPassword">Digite sua senha</label>
+              <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+            </div>
+            
+            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>  
 
 
 
@@ -115,12 +140,8 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
+  <!-- chama arquivo de funções js -->
+  <script src="js/index.js"></script>  
+  <script src="js/sidebar.js"></script>       
 
-  <!-- Menu Toggle Script--> 
-  <script>
-          $("#menu-toggle").click(function(e) {
-          e.preventDefault();
-          $("#wrapper").toggleClass("toggled");
-          });
-  </script>        
         </body>
