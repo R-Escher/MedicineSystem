004259dcd005181b1dfe544b8e6093a067dfb970
R-Escher <38119826+R-Escher@users.noreply.github.com>
2019-06-17 10:46:45 -0300

    Add files via upload

---
+++ laboratorio.php
@@ -0,0 +1,23 @@
+<?php include 'index-include/sidenav_laboratorio.php'; ?>
+<!-- Tags em aberto: <html>, <body>, <div class="d-flex" id="wrapper">, <div id="page-content-wrapper"> -->
+
+
+
+
+
+
+
+
+
+
+</div> <!-- / id="page-content-wrapper" | todo o conteúdo deve ficar aqui dentro -->
+</div> <!-- / class="d-flex" id="wrapper" -->
+  <!-- Javascript/Jquery functions for Bootstrap internal use -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
+  <!-- chama arquivo de funções js 
+  <script src="js/index.js"></script> -->     
+  <script src="js/sidebar.js"></script>  
+
+        </body>
---
+++ medico.php
@@ -0,0 +1,67 @@
+<?php include 'index-include/sidenav_medico.php'; ?>
+<!-- Tags em aberto: <html>, <body>, <div class="d-flex" id="wrapper">, <div id="page-content-wrapper"> -->
+
+
+<div class="container-fluid" style="padding: 30px 20px;">
+
+    <!-- botao de adicionar e input de busca -->
+    <div class="row" >
+        <div class="col-sm"><button class="btn btn-dark"><i class="fas fa-user-plus"></i></button></div>
+        <div class="row col-sm" style="padding: 0;">
+            <input type="email" class="form-control col-10" name="" id="">
+            <button class="btn btn-dark col-1.5"><i class="fas fa-search"></i></button>
+        </div>
+    </div>
+
+    <!-- tabela -->
+    <div>
+        <table class="table table-hover table-bordered table-striped table-dark text-center">
+        <thead>
+            <tr>
+            <th scope="col">PACIENTE</th>
+            <th scope="col">DATA</th>
+            <th scope="col">TELEFONE</th>
+            <th scope="col">E-MAIL</th>
+            <th scope="col">RECEITA</th>
+            <th scope="col">REQUISIÇÃO DE EXAME</th>
+            <th scope="col">OBSERVAÇÃO</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <td>Markasdasd asdadasdasdasdaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa</td>
+                <td>Otto</td>
+                <td>@arkasdasdasdada sdasdasdaaa aaaaaaaaaaaaaaaaaaaaaaaaaaa</td>
+                <td>arkasdasdasdadasd asdasda aaaaaaaaaaaaa aaaaaaaaaaaaaaaa</td>
+                <td>asdasdasd</td>
+                <td>llllllllllllllllllll</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+</div> <!-- / id="page-content-wrapper" | todo o conteúdo deve ficar aqui dentro -->
+</div> <!-- / class="d-flex" id="wrapper" -->
+  <!-- Javascript/Jquery functions for Bootstrap internal use -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
+  <!-- chama arquivo de funções js 
+  <script src="js/index.js"></script> -->     
+  <script src="js/sidebar.js"></script>  
+
+        </body>
