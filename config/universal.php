286fa8fd817391a6159731f50fbe6a887fae867c
R-Escher <38119826+R-Escher@users.noreply.github.com>
2019-06-17 21:52:53 -0300

    Add files via upload

---
+++ config/universal.php
@@ -0,0 +1,77 @@
+<?php 
+
+$universal = new universal;
+
+class universal{
+
+    protected $paciente;
+    protected $medico;
+    protected $laboratorio;
+    //protected $admin;
+
+    public function __construct(){
+
+        $this->paciente = new Paciente;
+        $this->medico = new Medico;
+        $this->laboratorio = new Laboratorio;
+
+    }
+
+    public function mostrarConsultas($chavePrimaria, $pessoa){
+        # ChavePrimária vai o CPF ou CRM, dependendo de quem pede a função.
+        # Pessoa determina se é para buscar consulta baseado no MEDICO ou PACIENTE (CRM/CPF)
+        #
+        libxml_use_internal_errors(true);
+        $xml_consultas = simplexml_load_file("dados/consultas.xml");    
+        if ($xml_consultas === false) {
+            echo "Erro no XML Consultas: ";
+            foreach (libxml_get_errors() as $error) {
+                echo "<br>", $error->message;
+            }
+        }elseif ($pessoa == "paciente"){
+
+            foreach ($xml_consultas->children() as $c) {
+                if ($c->cpf == $chavePrimaria) {
+                
+                }
+            }
+        }elseif ($pessoa == "medico"){
+
+            
+            foreach ($xml_consultas->children() as $c) {
+                $paciente = $this->paciente->buscapaciente($c->paciente);
+                if ($c->medico == $chavePrimaria) {
+                    
+                    #echo $c->paciente;
+                    $consulta = 
+                    '<tr>
+                        <th>'.$paciente->getNome().'</th>
+                        <td>'.$c->data.'</td>
+                        <td>'.$paciente->getTelefone().'</td>
+                        <td>'.$paciente->getEmail().'</td>
+                        <td>'.$c->receita.'</td>
+                        <td>'.$c->requisicao.'</td>
+                        <td>'.$c->observacoes.'</td>
+                    </tr>
+                        
+                    ';
+                    echo $consulta;
+                }
+            } 
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+?>
