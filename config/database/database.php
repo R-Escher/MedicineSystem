3cb56e8a60ea4d69ee58d30babeb518f5a3bbec6
r-escher <rafael-escher@hotmail.com>
2019-09-18 08:43:17 -0300

    finalizado metodos do DB

--- config/database/database.php
+++ config/database/database.php
@@ -1,6 +1,9 @@
 <?php
     $DB = new DB;
+
     $database = $DB::_conectaDB();
+
+    
     class DB{   
         public static $database;
         public static $e;
@@ -15,6 +18,8 @@
             }
             return self::$database;
         }
+
+        public function select($what, $from, )
 
         public function selectAll($from){
             $query = self::$database->prepare("SELECT * FROM ?");
@@ -34,87 +39,5 @@
             return $rows;
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public function verifica($tagId){
-            /*
-            * VERIFICA SE TAG ESTÁ NO DATABASE.
-            * RETORNA NOME SE ESTIVER,
-            * RETORNA NULL CASO CONTRÁRIO.
-            */
-            $query = self::$database->prepare("SELECT nome FROM cadastros WHERE tagId = ?");
-            $query->bindParam(1, $tagId);
-            $query->execute();
-            $row = $query->fetch(PDO::FETCH_OBJ);
-            if ($row==null){
-                return null;
-            } else {
-                return $row->nome;
-            }           
-        }
-        public function cadastra($tagId, $nome){
-            /*
-            * CADASTRA USUARIO NO DATABASE.
-            * RETORNA TRUE SE CADASTRAR,
-            * RETORNA FALSE CASO CONTRARIO.
-            */
-            if ($this->verifica($tagId) == null){
-                $query = self::$database->prepare("INSERT INTO cadastros (tagId, nome) VALUES (:tagId, :nome)");
-                $query->execute(array(":tagId" => $tagId, ":nome" => $nome));
-                return true; // CADASTRADO
-            }
-            else{
-                return false; // USUARIO JA EXISTE
-            }
-            
-        }
-        public function registra($tagId){ //id, tagId, nome, data, estado
-            /*
-            * REGISTRA USUARIO EM TABELA DE REGISTROS (LOG).
-            * RETORNA FALSE SE USUARIO NÃO EXISTE,
-            * RETORNA TRUE CASO REGISTRO DE CERTO.
-            */
-            $nome = $this->verifica($tagId);
-            if ($nome == null){
-                return false;
-            }
-            date_default_timezone_set('America/Sao_Paulo');
-            $data = date("Y-m-d H:i:s");
-            
-            $query = self::$database->prepare("SELECT estado FROM registros WHERE tagId = ?");
-            $query->bindParam(1, $tagId);
-            $query->execute();
-            $rows = $query->fetchAll(PDO::FETCH_OBJ);
-            
-            # VERIFICAÇÃO NECESSARIA PARA QUANDO NAO HA REGISTROS NA TABELA - IGNORAR.
-            $estado = end($rows);
-            if (gettype($estado)!='boolean'){
-                $estado = $estado->estado;
-            }            
-            
-            if ($estado == 0){ // se não está
-                $estado = 1;   // então vai entrar.
-            }else{
-                $estado = 0;
-                
-            }
-            $query = self::$database->prepare("INSERT INTO registros (tagId, nome, data, estado) VALUES (:tagId, :nome, :data, :estado)");
-            $query->execute(array(":tagId" => $tagId, ":nome" => $nome, ":data" => $data, ":estado" => $estado));
-            return true;
-        }
     }
 ?>
